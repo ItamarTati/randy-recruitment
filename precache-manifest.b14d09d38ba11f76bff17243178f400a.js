54471e3535671f3d7c2cb0c9c014c00b9d5bbcfe
Itamar Tati <itamar.tati1994@gmail.com>
2019-08-29 16:19:30 +0100

    Updates

---
+++ precache-manifest.b14d09d38ba11f76bff17243178f400a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55eba414da953d4217a441dcf06a29e6",
+    "url": "/randy-recruitment/index.html"
+  },
+  {
+    "revision": "178d0b53bf7f1aa5a22e",
+    "url": "/randy-recruitment/static/css/main.8e7d0516.chunk.css"
+  },
+  {
+    "revision": "fa7a88e3ab34d57b04ca",
+    "url": "/randy-recruitment/static/js/2.dd11fb47.chunk.js"
+  },
+  {
+    "revision": "178d0b53bf7f1aa5a22e",
+    "url": "/randy-recruitment/static/js/main.3aa6bbf1.chunk.js"
+  },
+  {
+    "revision": "624eb3dcc5f9db6de947",
+    "url": "/randy-recruitment/static/js/runtime~main.08cc687c.js"
+  },
+  {
+    "revision": "dc4f55ea9e26ddcdbe32cfeafbdeaa99",
+    "url": "/randy-recruitment/static/media/background-image.dc4f55ea.png"
+  }
+]);
